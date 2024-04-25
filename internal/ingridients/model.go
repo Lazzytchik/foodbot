cec27b166b32a2a0ea59620b13226a94adcae551
Lazzytchik <Lazzytchik@users.noreply.github.com>
2024-04-25 05:54:03 +0300

    ingridients shit rename

--- internal/ingridients/model.go
+++ internal/ingridients/model.go
@@ -1,13 +1,13 @@
 package ingridients
 
 type Ingridient struct {
-	ID   int    `db:"id"`
-	Name string `db:"name"`
+	ID   int    `db:"id" json:"id"`
+	Name string `db:"name" json:"name"`
 
-	Calories   float32 `db:"calories"`
-	Nutritions float32 `db:"nutritions"`
-	Fats       float32 `db:"fats"`
-	Carbs      float32 `db:"carbs"`
-	Water      float32 `db:"water"`
-	Fibers     float32 `db:"fiber"`
+	Calories   float32 `db:"calories" json:"calories"`
+	Nutritions float32 `db:"nutritions" json:"nutritions"`
+	Fats       float32 `db:"fats" json:"fats"`
+	Carbs      float32 `db:"carbs" json:"carbs"`
+	Water      float32 `db:"water" json:"water"`
+	Fibers     float32 `db:"fibers" json:"fibers"`
 }
